27b6f5d81e6d7655e83c60fae44365108a973e69
eduardobaltazar92 <37169978+eduardobaltazar92@users.noreply.github.com>
2018-03-08 00:10:37 -0600

    Add files via upload

---
+++ Untitled.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+
+
+# In[20]:
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set_style("whitegrid")
+get_ipython().run_line_magic('matplotlib', 'inline')
+
+
+# In[44]:
+
+
+df = pd.read_csv('data/nominas.csv', index_col='Bib')
+df.head(5)
+df.iloc[:,8:20].head(5)
+
